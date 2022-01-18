1dfef17414f462a9672663247703f8b0e92243ae
thomasbititude <thomas@bititude.com>
2022-01-18 14:28:23 +0530

    event binding added

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,17 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-allowNewServer = false;
-  constructor() { 
-    setTimeout(()=>{
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+
+  constructor() {
+    setTimeout(() => {
       this.allowNewServer = true;
-    },2000)
+    }, 2000);
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onCreateServer(){
+    this.serverCreationStatus = 'new server created';
+  }
 }
