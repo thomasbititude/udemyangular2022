1a9452d2dfe408f092d5b7ba775d63bd38722657
thomasbititude <thomas@bititude.com>
2022-01-18 20:38:16 +0530

    Two-Way-DataBinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
-serverName = "";
+serverName = "TestServer";
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -20,7 +20,7 @@ serverName = "";
   onCreateServer(){
     this.serverCreationStatus = 'new server created';
   }
-  onUpdateServerName(event:any){
+  onUpdateServerName(event:Event){
 console.log(event);
 this.serverName = (<HTMLInputElement>event.target).value;
   }
